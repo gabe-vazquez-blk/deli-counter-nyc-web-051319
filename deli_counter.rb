64b537184a6c8b6a01d77231736ad0b6ec329459
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 19:50:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 def line(katz_deli)
   customers = []
-  if katz_deli.length() == 0 
+  if katz_deli.length == 0 
     puts "The line is currently empty."
   else
     katz_deli = katz_deli.each_with_index.map do |customer, position|
       "#{position + 1}. #{customer}"
     end
-  end
-  katz_deli = katz_deli.join(" ")
-  puts "The line is currently: " + katz_deli
+    katz_deli = katz_deli.join(" ")
+    puts "The line is currently: " + katz_deli
+    
 end
