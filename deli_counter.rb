36eae018de0b5a6da3d1fc4ccc9d8c8f560458cc
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 19:49:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli = katz_deli.each_with_index.map do |customer, position|
-      "#{position + 1}. #{customer} "
+      "#{position + 1}. #{customer}"
     end
   end
-  puts katz_deli 
-  #= katz_deli.join(" ")
-  #katz_deli
+  katz_deli = katz_deli.join(" ")
+  puts "The line is currently: " + katz_deli
 end
