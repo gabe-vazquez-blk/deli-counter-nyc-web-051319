e26c5115c11db36832b5ae30bdf707d0c1e72282
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 19:27:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli)
-  customers
+  customers = []
   if katz_deli.length() == 0 
     puts "The line is currently empty."
   else
@@ -10,5 +10,7 @@ def line(katz_deli)
     customers = customers.join(" ")
     
   end
+  
+  puts "The line is currently: " + customers
   
 end
