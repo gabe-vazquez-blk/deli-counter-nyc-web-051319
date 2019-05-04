81277bfd06249781217e6576a22ff1486b589207
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 19:46:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.length() == 0 
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index.map do |customer, position|
+    katz_deli.each_with_index.map! do |customer, position|
       "#{position + 1}. #{customer} "
     end
   end
