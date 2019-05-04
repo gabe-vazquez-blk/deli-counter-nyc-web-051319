46d80931a15365ef38b39501c2275e269fb366e6
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 20:11:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,6 +25,6 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts katz_deli
+    puts "Currently serving #{katz_deli[0]}."
   end
 end
